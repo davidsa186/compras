2884955ecd27c48c2db384dd662eda6a68bc91f4
davidsa186 <david.sa86@yahoo.com>
2016-05-17 15:57:58 -0500

    Plantilla

---
+++ CarritoDeCompras/upb.tabd.entidades/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upb.tabd.entidades
+{
+    public class Producto
+    {
+        public int Id_Producto { get; set; }
+        public string Nombre_Producto { get; set; }
+        public int Precio { get; set; }
+        public int Stock { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public Proveedor Proveedor { get; set; }
+
+    }
+}
---
+++ CarritoDeCompras/upb.tabd.entidades/Proveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upb.tabd.entidades
+{
+    public class Proveedor
+    {
+        public int Id_Proveedor { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Sitio_Web { get; set; }
+    }
+}
